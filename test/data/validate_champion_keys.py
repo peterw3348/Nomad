441aa78f1fbcbd4f5d16e8d990d771f9b26c38b5
PeterW. <peter2451995@gmail.com>
2025-03-03 20:03:13 -0500

    Added data to test folder
    Compares 3rd party data to authoritive data provided by endpoint

---
+++ test/data/validate_champion_keys.py
@@ -0,0 +1,18 @@
+import json
+from src.api import paths
+
+with open("champions.json", "r") as file:
+    champions = json.load(file)
+
+with open("updated_champion_ratings_with_names.json", "r") as file:
+    champion_ratings = json.load(file)
+
+name_to_id = {v: k for k, v in champions.items()}
+
+for champ_id, data in champion_ratings.items():
+    expected_name = data.get("name")
+    actual_name = champions.get(champ_id)
+
+    assert actual_name == expected_name, f"Mismatch for key {champ_id}: Expected {expected_name}, Found {actual_name}"
+
+print("All champion keys and names are correct!")
